84920730438d2d18aabd2ad7f75bb60e9c7ed773
dininta <dinintaannisa@yahoo.com>
2014-11-14 21:26:48 +0700

    Update jam.h

--- jam.h
+++ jam.h
@@ -2,6 +2,7 @@
 
 #ifndef JAM_C
 #define JAM_C
+#include "boolean.h"
 
 /* Definisi TYPE JAM <HH:MM:SS> */
 typedef struct {
@@ -16,13 +17,17 @@ JAM MakeJAM (int HH, int MM, int SS);
 // Prekondisi : HH, MM, SS valid untuk membentuk JAM
 
 /*** Selektor ***/
-#define Hour(J) (J).HH
-#define Minute(J) (J).MM
-#define Second(J) (J).SS
+#define Jam(J) (J).HH
+#define Menit(J) (J).MM
+#define Detik(J) (J).SS
 
 /*** FUNGSI LAIN ***/
+boolean IsJamEqual (JAM J1, JAM J2);
+// Menghasilkan true jika J1 = J2
 JAM JamEarly (JAM J1, JAM J2);
 // Menghasilkan jam yang lebih dulu di antara J1 dan J2
 // Prekondisi : J1 != J2
+void PrintJam (JAM J);
+// Menampilkan jam pada layar dengan format HH:MM:SS
 
 #endif
