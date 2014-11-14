529a640b2bcdb43e8e29cb41a6f8def7ef4b85a0
dininta <dinintaannisa@yahoo.com>
2014-11-14 21:27:18 +0700

    Update jam.c

--- jam.c
+++ jam.c
@@ -1,6 +1,8 @@
 /* ADT JAM */
 
 #include "jam.h"
+#include "boolean.h"
+#include <stdio.h>
 
 /*** Konstruktor: Membentuk sebuah JAM dari komponen-komponennya ***/
 JAM MakeJAM (int HH, int MM, int SS)
@@ -18,22 +20,32 @@ JAM MakeJAM (int HH, int MM, int SS)
 }
 
 /*** FUNGSI LAIN ***/
+boolean IsJamEqual (JAM J1, JAM J2)
+// Menghasilkan true jika J1 = J2
+{
+	return((Jam(J1)==Jam(J2))&&(Menit(J1)==Menit(J2))&&(Detik(J1)==Detik(J2)));
+}
 JAM JamEarly (JAM J1, JAM J2)
 // Menghasilkan jam yang lebih dulu di antara J1 dan J2
 // Prekondisi : J1 != J2
 {
-	if (Hour(J1)<Hour(J2))
+	if (Jam(J1)<Jam(J2))
 		return (J1);
-	else if (Hour(J1)>Hour(J2))
+	else if (Jam(J1)>Jam(J2))
 			return (J2);
 		 else
-			if (Minute(J1)<Minute(J2))
+			if (Menit(J1)<Menit(J2))
 				return (J1);
-			else if (Minute(J1)>Minute(J2))
+			else if (Menit(J1)>Menit(J2))
 					return (J2);
 				 else
-					if (Second(J1)<Second(J2))
+					if (Detik(J1)<Detik(J2))
 						return (J1);
 					else
 						return (J2);
+}
+void PrintJam (JAM J)
+// Menampilkan jam pada layar dengan format HH:MM:SS
+{
+	printf("%02i:%02i:%02i",Jam(J),Menit(J),Detik(J));
 }
